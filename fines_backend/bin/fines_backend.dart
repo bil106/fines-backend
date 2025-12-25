905057875b0aa98aac273f1f49a2568f83079dd7
mbeloded <mbeloded@gmail.com>
2025-12-25 22:20:36 +0200

    Initial backend

---
+++ fines_backend/bin/fines_backend.dart
@@ -0,0 +1,5 @@
+import 'package:fines_backend/fines_backend.dart' as fines_backend;
+
+void main(List<String> arguments) {
+  print('Hello world: ${fines_backend.calculate()}!');
+}
---
+++ fines_backend/test/fines_backend_test.dart
@@ -0,0 +1,8 @@
+import 'package:fines_backend/fines_backend.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
